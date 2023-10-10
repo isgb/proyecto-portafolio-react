d0079ed4e9ddd7a66859d63ed49d36ec02da905f
oscar <oscar.vasquez@zentro.com.mx>
2023-10-10 16:44:16 -0600

    avances

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+   <footer className='footer'>
+        Portafolio Isaias S.G.B. &copy; React JS 2023
+    </footer>
+  )
+}
---
+++ src/router/MisRutas.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom'
+import { Inicio } from '../components/Inicio'
+import { Portafolio } from '../components/Portafolio'
+import { Servicios } from '../components/Servicios'
+import { Curriculum } from '../components/Curriculum'
+import { Contacto } from '../components/Contacto'
+import { HeaderNav } from '../components/layout/HeaderNav'
+import { Footer } from '../components/layout/Footer'
+
+export const MisRutas = () => {
+    return (
+        <BrowserRouter>
+
+            {/* HEADER Y NAVEGACION */}
+            <HeaderNav />
+            {/* CONTENIDO CENTRAL */}
+            <section className='content'>
+                <Routes>
+                    <Route path='/' element={<Inicio />} />
+                    <Route path='/inicio' element={<Inicio />} />
+                    <Route path='/portafolio' element={<Portafolio />} />
+                    <Route path='/servicios' element={<Servicios />} />
+                    <Route path='/curriculum' element={<Curriculum />} />
+                    <Route path='/contacto' element={<Contacto />} />
+                </Routes>
+            </section>
+
+
+            {/* FOOTER */}
+            <Footer />
+        </BrowserRouter>
+    )
+}
