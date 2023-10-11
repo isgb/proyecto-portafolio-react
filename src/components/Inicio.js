a13ef9e1eb989c4973581999c9cc42ca8eb54921
oscar <oscar.vasquez@zentro.com.mx>
2023-10-11 11:01:29 -0600

    avances

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div>
+      <h1>
+        Hola, soy Victor Robles y soy Desarrollador Web en Madrid, 
+        y ofrezco mis servicios de programación y desarrollo en todo 
+        tipo de proyectos web.
+      </h1>
+      
+      <h2>
+        Te ayudo a crear tu sitio o aplicación web, tener más visibilidad
+        y relevancia en internet.
+      </h2>
+    </div>
   )
 }
