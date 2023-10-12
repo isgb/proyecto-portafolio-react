960babd715541e54e47f9b91436d5de92eebb6b4
oscar <oscar.vasquez@zentro.com.mx>
2023-10-12 17:10:47 -0600

    avances

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { trabajos } from '../data/trabajos'
+import { Link } from 'react-router-dom'
 
 export const Portafolio = () => {
+
+  // console.log(trabajos)
   return (
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
+
+      <section className='works'>
+      {
+        trabajos.map(trabajo => (
+          <article key={trabajo.id} className='work-item'>
+            <div className='mask'>
+              <img src={"/imagenes/"+trabajo.id+".png"}/>
+            </div>
+            <span>{trabajo.categorias}</span>
+            <h2><Link to={"/proyecto/"+trabajo.id}>{trabajo.nombre}</Link></h2>
+            <h3>{trabajo.tecnologias}</h3>
+          </article>
+        ))
+      }
+      </section>
     </div>
   )
 }
---
+++ src/data/trabajos.js
@@ -0,0 +1,42 @@
+export const trabajos = [
+    {
+        'id' : 'imagen_1',
+        'nombre' : 'Victor Robles WEB',
+        'url' : 'victorroblesweb.es',
+        'tecnologias' : 'HTML, CSS, JS, PHP, MySQL',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        'id' : 'imagen_2',
+        'nombre' : 'Facebook',
+        'url' : 'facebook.com',
+        'tecnologias' : 'React, Mongo, Node',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        'id' : 'imagen_3',
+        'nombre' : 'Twitter',
+        'url' : 'twitter.com',
+        'tecnologias' : 'HTML, CSS',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        'id' : 'imagen_4',
+        'nombre' : 'LinkedIn',
+        'url' : 'linkedin.com',
+        'tecnologias' : 'HTML, CSS, JS, PHP, Laravel',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        'id' : 'imagen_5',
+        'nombre' : 'Udemy',
+        'url' : 'udemy.com',
+        'tecnologias' : 'MEAN Stack',
+        'categorias' : 'Desarrollo Web',
+        'descripcion' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+]
